bf6da7796c8327e645004a7a7bc7cad839d0699c
Thaker25 <devanshthaker4@gmail.com>
2019-03-05 01:05:10 +0530

    Initial commit

---
+++ Fhandling.py
@@ -0,0 +1,16 @@
+#f=open("hello.txt","r")
+#print(f.read())
+#f.close()
+
+
+#f=open("hello.txt","a")
+#f.write("Thaker")
+#f.close()
+
+f=open("user.txt","w")
+a= input("Enter input")
+f.write(a)
+f.close()
+
+
+
